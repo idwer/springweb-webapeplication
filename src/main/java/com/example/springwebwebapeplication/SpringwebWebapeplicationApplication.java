21fc0fa0eb779a3db0b217c7eabc9dfee32c515e
idwer <idwer@localhost>
2022-12-26 14:43:54 +0100

    initial commit, store generated project

---
+++ src/main/java/com/example/springwebwebapeplication/SpringwebWebapeplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebwebapeplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebWebapeplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebWebapeplicationApplication.class, args);
+    }
+
+}
